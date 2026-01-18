0a6f99381d843bb97498a4644d00620826e32f99
Mihai <mihai@sunriserobotics.co>
2026-01-18 10:47:34 +0100

    feat: Add main entry point vex.nim (exports all core modules)

---
+++ src/vex/vex.nim
@@ -0,0 +1,10 @@
+## VEX - Vector & Hex Scene Graph Library
+## High-performance 2D scene graph for Nim
+
+import ./core/types
+import ./core/transform
+import ./core/events
+
+export types
+export transform
+export events
